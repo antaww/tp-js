fc38654cf490d0336a78a64e912f94e20fc3d32e
antaww <anta.pizzetta@gmail.com>
2022-05-10 16:34:49 +0200

    questions working

--- index.js
+++ index.js
@@ -23,3 +23,39 @@ function nextFunc() {
 next.addEventListener('click', () => {
     nextFunc();
 })
+
+function submitAnswers() {
+    var total = 2;
+    var score = 0;
+
+    //Get user input
+    var q1 = document.forms['quizForm']['q1'].value;
+    var q2 = document.forms['quizForm']['q2'].value;
+
+    // Validation
+    for (var i = 1; i <= total; i++) {
+        if (eval('q' + i) === null || eval('q' + i) == '') {
+            alert('You missed question ' + i);
+            return false;
+        }
+    }
+
+    // Set correct answers
+    var answers = ["b", "a"];
+
+    for (var i = 1; i <= total; i++) {
+        // Check answers
+        if (eval('q' + i) == answers[i - 1]) {
+            score++;
+        }
+    }
+    console.log(score);
+
+    // Display results
+    var results = document.getElementById('results');
+    results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
+    //alert('You scored ' + score + ' out of ' + total);
+
+    return false;
+}
+
