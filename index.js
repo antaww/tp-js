5488c3df3bc59eeae82981b4669ea55a851f80dd
antaww <anta.pizzetta@gmail.com>
2022-05-10 15:53:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var imgs = document.querySelectorAll('.slider-1 .sliderContent div');
+var prev = document.querySelector('.slider-1 .prev')
+var next = document.querySelector('.slider-1 .next')
+var sliderWraper = document.querySelector('.slider-1 .sliderContent')
+
+var pos = 0;
+var nbSlide = imgs.length;
+
+
+window.addEventListener("keydown", event => {
+    if (event.key == "ArrowRight") {
+        nextFunc()
+    }
+})
+
+function nextFunc() {
+    pos = pos - 100;
+    if (pos < ((-nbSlide + 1) * 100)) {
+        sliderWraper.style.left = 0 + 'vw';
+        pos = 0;
+    } else {
+        sliderWraper.style.left = pos + 'vw';
+    }
+}
+
+next.addEventListener('click', () => {
+    nextFunc();
+})
