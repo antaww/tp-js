7b097dfbe298040e545b512312f24baacf21b690
antaww <anta.pizzetta@gmail.com>
2022-05-10 17:46:03 +0200

    checkbox not working

--- index.js
+++ index.js
@@ -7,26 +7,29 @@ var pos = 0;
 var nbSlide = imgs.length;
 
 
-var total = 2;
-var score = 0;
 
-//Get user input
-var q1 = document.forms['quizForm']['q1'].value;
-var q2 = document.forms['quizForm']['q2'].value;
 
 
 
 // Set correct answers
-var answers = ["a", "a"];
+var answers = ["a", "a", "a"];
 
 function submitAnswers() {
+    var total = 3;
+    var score = 0;
+
+//Get user input
+    var q1 = document.forms['quizForm']['q1'].value;
+    var q2 = document.forms['quizForm']['q2'].value;
+    var q3 = document.querySelectorAll('input[name="q3"]:checked');
 
     for (var i = 1; i <= total; i++) {
         // Check answers
-        if (eval('q' + i) == answers[i - 1]) {
+        if (eval('q' + i) === answers[i - 1]) {
             score++;
         }
     }
+    console.log(q1,q2,q3);
     console.log(score);
 
     // Display results
@@ -43,8 +46,6 @@ window.addEventListener("keydown", event => {
 })
 
 function nextFunc() {
-    // Validation
-    //if
     pos = pos - 100;
     if (!(pos < ((-nbSlide + 1) * 100))) {
         sliderWraper.style.left = pos + 'vw';
