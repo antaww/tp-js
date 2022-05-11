5fa806af4458581a60c11880d73f98692c422e93
antaww <anta.pizzetta@gmail.com>
2022-05-11 15:59:31 +0200

    done

--- TP-Animation/index.js
+++ TP-Animation/index.js
@@ -1,2 +1,6 @@
-var seqA = TweenMax.to('.seqA',5,{repeat:-1,y:-46680,ease:SteppedEase.config(120)});
-var seqB = TweenMax.to('.seqB',5,{repeat:-1,y:-39864,ease:SteppedEase.config(120)});
+TweenMax.to('.seqA',5,{repeat:-1,y:-46680,ease:SteppedEase.config(120)});
+//TweenMax to :
+//1. target element (class)
+//2. duration (seconds)
+//3. properties : repeat (-1 = infinite), y (img size), ease (img total frames)
+TweenMax.to('.seqB',5,{repeat:-1,y:-39864,ease:SteppedEase.config(132)});
