eb2c012c650eef696b48f00d4341b733ba6e8fc6
Bahsiik <o.mist1303@gmail.com>
2022-05-11 14:00:41 +0200

    working hero slide

--- TP-Quizz/index.js
+++ TP-Quizz/index.js
@@ -77,24 +77,46 @@ next.addEventListener('click', () => {
 
 //check it every second
 setInterval(() => {
-    const checkedA = document.getElementById("q3a").checked;
-    document.getElementsByClassName("genji")[0].style.filter = `grayscale(${checkedA ? 0 : 100}`;
-    document.getElementsByClassName("hanzo")[0].style.filter = `grayscale(${checkedA ? 0 : 100}`;
+    const checked3A = document.getElementById("q3a").checked;
+    document.getElementsByClassName("genji")[0].style.filter = `grayscale(${checked3A ? 0 : 100}`;
+    document.getElementsByClassName("hanzo")[0].style.filter = `grayscale(${checked3A ? 0 : 100}`;
 
-    const checkedB = document.getElementById("q3b").checked;
-    document.getElementsByClassName("ana")[0].style.filter = `grayscale(${checkedB ? 0 : 100}`;
-    document.getElementsByClassName("pharah")[0].style.filter = `grayscale(${checkedB ? 0 : 100}`;
+    const checked3B = document.getElementById("q3b").checked;
+    document.getElementsByClassName("ana")[0].style.filter = `grayscale(${checked3B ? 0 : 100}`;
+    document.getElementsByClassName("pharah")[0].style.filter = `grayscale(${checked3B ? 0 : 100}`;
 
-    const checkedC = document.getElementById("q3c").checked;
-    document.getElementsByClassName("junkrat")[0].style.filter = `grayscale(${checkedC ? 0 : 100}`;
-    document.getElementsByClassName("roadhog")[0].style.filter = `grayscale(${checkedC ? 0 : 100}`;
+    const checked3C = document.getElementById("q3c").checked;
+    document.getElementsByClassName("junkrat")[0].style.filter = `grayscale(${checked3C ? 0 : 100}`;
+    document.getElementsByClassName("roadhog")[0].style.filter = `grayscale(${checked3C ? 0 : 100}`;
 
-    const checkedD = document.getElementById("q3d").checked;
-    document.getElementsByClassName("torbjorn")[0].style.filter = `grayscale(${checkedD ? 0 : 100}`;
-    document.getElementsByClassName("brigitte")[0].style.filter = `grayscale(${checkedD ? 0 : 100}`;
+    const checked3D = document.getElementById("q3d").checked;
+    document.getElementsByClassName("torbjorn")[0].style.filter = `grayscale(${checked3D ? 0 : 100}`;
+    document.getElementsByClassName("brigitte1")[0].style.filter = `grayscale(${checked3D ? 0 : 100}`;
 
-    const checkedE = document.getElementById("q7c").checked;
-    document.getElementsByClassName("reinhardt")[0].style.filter = `grayscale(${checkedE ? 0 : 100}`;
+    const checked7a = document.getElementById("q7a").checked;
+    document.getElementsByClassName("doomfist")[0].style.filter = `grayscale(${checked7a ? 0 : 100}`;
+
+    const checked7b = document.getElementById("q7b").checked;
+    document.getElementsByClassName("brigitte2")[0].style.filter = `grayscale(${checked7b ? 0 : 100}`;
+
+    const checked7c = document.getElementById("q7c").checked;
+    document.getElementsByClassName("reinhardt")[0].style.filter = `grayscale(${checked7c ? 0 : 100}`;
+
+    const checked7d = document.getElementById("q7d").checked;
+    document.getElementsByClassName("bastion")[0].style.filter = `grayscale(${checked7d ? 0 : 100}`;
+
+    const checked7e = document.getElementById("q7e").checked;
+    document.getElementsByClassName("echo")[0].style.filter = `grayscale(${checked7e ? 0 : 100}`;
+
+    const checked7f = document.getElementById("q7f").checked;
+    document.getElementsByClassName("zarya")[0].style.filter = `grayscale(${checked7f ? 0 : 100}`;
+
+    const checked7g = document.getElementById("q7g").checked;
+    document.getElementsByClassName("orisa")[0].style.filter = `grayscale(${checked7g ? 0 : 100}`;
+
+    const checked7h = document.getElementById("q7h").checked;
+    document.getElementsByClassName("mei")[0].style.filter = `grayscale(${checked7h ? 0 : 100}`;
+
 }, 400);
 
 
