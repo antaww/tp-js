0e1ee43038d23040f8dddb984f9e43617211441b
antaww <anta.pizzetta@gmail.com>
2022-05-11 15:48:46 +0200

    seqA animated

--- TP-Animation/index.js
+++ TP-Animation/index.js
@@ -1,1 +1,2 @@
-console.log("hh")
+var seqA = TweenMax.to('.seqA',5,{repeat:-1,y:-46680,ease:SteppedEase.config(120)});
+var seqB = TweenMax.to('.seqB',5,{repeat:-1,y:-39864,ease:SteppedEase.config(120)});
