2a0fcb863f61bf50f7a254dee4879c0bf742ecd1
antaww <anta.pizzetta@gmail.com>
2022-05-11 16:23:06 +0200

    little dark freeze fixed

--- TP-Animation/index.js
+++ TP-Animation/index.js
@@ -1,6 +1,6 @@
-TweenMax.to('.seqA',5,{repeat:-1,y:-46680,ease:SteppedEase.config(120)});
+TweenMax.to('.seqA_container',5,{repeat:-1,backgroundPosition:"0px -46680px",ease:SteppedEase.config(120)});
 //TweenMax to :
 //1. target element (class)
 //2. duration (seconds)
 //3. properties : repeat (-1 = infinite), y (img size), ease (img total frames)
-TweenMax.to('.seqB',5,{repeat:-1,y:-39864,ease:SteppedEase.config(132)});
+TweenMax.to('.seqB_container',5,{repeat:-1,backgroundPosition:"0px -39864px",ease:SteppedEase.config(132)});
