f78ccd531b43082f5f9e7283dd482a947b8e3087
antaww <anta.pizzetta@gmail.com>
2022-05-17 11:54:54 +0200

    diamond gain calculation working + little animation with timeout

--- TP-Cash/index.js
+++ TP-Cash/index.js
@@ -8,7 +8,9 @@ let benefits = document.querySelector('.benefits');
 
 
 let wallet = 500;
-let gain = 0;
+let totalGain = 0;
+let result = [];
+
 
 const items = [
     "🍋",
@@ -20,18 +22,16 @@ const items = [
     "7️"
 ]
 
-function Init(){
+function Init() {
     coins.innerHTML = wallet;
 }
 
-window.addEventListener('click', function(e){
-    if(e.target.id.includes('spinner')){
+window.addEventListener('click', function (e) {
+    if (e.target.id.includes('spinner')) {
         let i = e.target.id.substring(7);
         SpinFunc(i);
     }
 });
-
-
 
 
 // Randomly select an item (the last item has a lower chance of being selected (50%))
@@ -45,14 +45,59 @@ function shuffle([...arr]) {
 }
 
 function SpinFunc(i) {
+    console.clear();
+    //clear doors
+    doors.forEach(door => {
+        door.innerHTML = '';
+    });
+    console.log("Wallet before spin : ", wallet);
+    console.log("Coins bet : ", i);
+    if (wallet - i < 0) {
+        console.log("You don't have enough money to play");
+        return
+    }
     wallet -= i;
     coins.innerHTML = wallet;
-// Shuffle the items and assign them to the doors
     let shuffledItems = shuffle(items);
     for (let i = 0; i < doors.length; i++) {
-        doors[i].innerHTML = shuffledItems[i];
-
+        //Wait 0.5s before displaying the next door
+        setTimeout(function () {
+            doors[i].innerHTML = shuffledItems[i];
+        }, i * 500);
     }
+    //Wait to for loop to finish
+    setTimeout(function () {
+        CalculateGain(i);
+        console.log("Wallet after spin : ", wallet);
+    }, doors.length * 500);
+}
+
+function CalculateGain(i) {
+    result = [];
+    let gain = 0;
+    for (let j = 0; j < doors.length; j++) {
+        result.push(doors[j].innerHTML);
+    }
+    console.log(result);
+    if (result.includes("💎")) {
+        let gemCount = result.filter(x => x === "💎").length;
+        console.log(gemCount, "💎");
+        if (gemCount === 1) {
+            totalGain += i * 0.5
+            gain += i * 0.5;
+        } else if (gemCount === 2) {
+            totalGain += i * 1;
+            gain += i * 1;
+        } else if (gemCount === 3) {
+            totalGain += i * 2.5;
+            gain += i * 2.5;
+        }
+    }
+    wallet += gain;
+    console.log("Total gain : ", totalGain);
+    console.log("Gain this round : ", gain);
+    coins.innerHTML = wallet;
+    benefits.innerHTML = totalGain;
 }
 
 Init();
